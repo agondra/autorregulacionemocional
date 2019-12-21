c442f3f8650de0ec2bccfba5e78a8adb0014275b
Alex <gonrualex@gmail.com>
2019-12-21 19:33:49 +0100

    auto-002 - Validaciones personalizadas

--- src/app/pages/signin/signin.page.ts
+++ src/app/pages/signin/signin.page.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { CustomValidators } from '../../validators/ValidatorPassword';
+import { ValidatorsDate } from '../../validators/ValidatorsDate';
 
 @Component({
   selector: 'app-signin',
@@ -20,11 +22,13 @@ export class SigninPage implements OnInit {
       email: ['', Validators.compose([Validators.required, Validators.email])],
       password: ['', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(30),Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!$_%*?&])[A-Za-z\d$@$!$_%*?&].{8,}')])],
       repeatPassword: ['', Validators.compose([Validators.required, Validators.minLength(8),Validators.maxLength(30),Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!$_%*?&])[A-Za-z\d$@$!$_%*?&].{8,}')])],
-      birthDate:['',Validators.compose([Validators.required])],
+      birthDate:['',Validators.compose([Validators.required, ValidatorsDate.validatorsDate])],
       name: ['', Validators.compose([Validators.required,Validators.minLength(3),Validators.maxLength(30)])],
       surname: ['', Validators.compose([Validators.required,Validators.minLength(3),Validators.maxLength(30)])]
       
     });
+
+    this.credentialsForm.get('repeatPassword').setValidators(CustomValidators.equals(this.credentialsForm.get('password')));
   }
  
   onSubmit() {
